1c89bf8ba4f3eca1415ffeb282463fa092410d53
Yiian Chen <115157724+Aoitok@users.noreply.github.com>
2024-12-11 16:57:42 +0800

    A6 complete

--- A6/tai-e/src/main/java/pascal/taie/analysis/pta/core/cs/selector/_2TypeSelector.java
+++ A6/tai-e/src/main/java/pascal/taie/analysis/pta/core/cs/selector/_2TypeSelector.java
@@ -43,19 +43,27 @@ public class _2TypeSelector implements ContextSelector {
 
     @Override
     public Context selectContext(CSCallSite callSite, JMethod callee) {
-        // TODO - finish me
-        return null;
+        return callSite.getContext();
     }
 
     @Override
     public Context selectContext(CSCallSite callSite, CSObj recv, JMethod callee) {
-        // TODO - finish me
-        return null;
+        Context context = recv.getContext();
+        Type type = recv.getObject().getContainerType();
+        int length = context.getLength();
+        if (length == 0) {
+            return ListContext.make(type);
+        }
+        return ListContext.make(context.getElementAt(length - 1), type);
     }
 
     @Override
     public Context selectHeapContext(CSMethod method, Obj obj) {
-        // TODO - finish me
-        return null;
+        Context context = method.getContext();
+        int length = context.getLength();
+        if (length == 0) {
+            return getEmptyContext();
+        }
+        return ListContext.make(context.getElementAt(length - 1));
     }
 }
